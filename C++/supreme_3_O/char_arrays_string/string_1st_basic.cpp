167784013251525f2e59f934af4bdbd8822be669
Lokesh Mapui <lokeshmapui88@gmail.com>
2024-07-19 19:01:19 +0530

    basic

---
+++ C++/supreme_3_O/char_arrays_string/string_1st_basic.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+                       // predefined function
+
+
+    string str1 = "lokesh";
+    string str2 = "mapui";
+    cout << str1.compare(str2) << endl;
+
+    // string name="hello jee kaise ho sare kya haal chal";
+    // string word="kaise";
+    // cout<<name.find(word);
+    // cout<<name.substr(3)<<endl;
+    // cout<<name.substr(0,37)<<endl;
+
+    // string fname="lokesh";
+    // string lname="mapui";
+    // string ans=fname+" " +lname;
+    // cout<<ans<<endl;
+
+    // string name="Maharana Pratap";
+    // name.clear();
+    // if(name.empty()){
+    //     cout<<"string is empty"<<endl;
+    // }
+    // else {
+    //     cout<<"string is not empty"<<endl;
+    // }
+
+    // auto it=name.begin();
+    // while( it!=name.end()){
+    //     cout<<*it<<" ";
+    //     it++;
+    // }
+    // cout<<endl;
+
+    // cout<<name[9]<<endl;
+    // cout<<name.at(0)<<endl;
+    // cout<<name.front()<<endl;
+    // cout<<name.back()<<endl;
+    // cout<<name.length()<<endl;
+
+    // string sentence;
+    // getline(cin, sentence, 'A');
+    // cout << sentence << endl;
+
+    // string str;
+    // cout<<"enter the input :"<<endl;
+    // cin>>str;
+    // cout<<"str :"<<str<<endl;
+
+    return 0;
+}
