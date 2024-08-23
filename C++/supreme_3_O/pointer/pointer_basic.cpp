8fde69d548ba13f4f5feb6b01d7411c57d752132
Lokesh Mapui <lokeshmapui88@gmail.com>
2024-08-23 18:20:12 +0530

    pointer

---
+++ C++/supreme_3_O/pointer/pointer_basic.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    // char ch[100]="Lokesh";
+    // char *cptr=ch;
+    // cout<<ch<<endl;
+    // cout<<&ch<<endl;
+    // cout<<ch[0]<<endl;
+    // cout<<&cptr<<endl;
+    // cout<<*cptr<<endl;
+    // cout<<*(cptr+3)<<endl;
+    // cout<<cptr<<endl;
+ 
+    // int arr[]={10,20,30,40};
+    // int *ptr=arr;
+    // cout<<arr<<endl;
+    // cout<<&arr<<endl;
+    // cout<<arr[0]<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<ptr<<endl;
+    // cout<<&ptr<<endl;
+    // cout<<*ptr<<endl;
+    // cout<<*arr+1<<endl;
+    // cout<<*(arr+1)<<endl;
+    // cout<<*(arr+3)<<endl;
+
+    // int arr[]={10,20,30,40};
+    // int *ptr=arr;
+    // ptr++;
+    // cout<<"hello"<<endl;
+
+
+    // int a=5;
+    // int*ptr=&a;
+    // cout<<a<<endl;
+    // cout<<&a<<endl;
+    // cout<<*a<<endl;
+    // cout<<ptr<<endl;
+    // cout<<&ptr<<endl;
+    // cout<<*ptr<<endl;
+    // cout<<ptr<<endl;
+
+    // char ch='a';
+    // char*charptr=&ch;
+    // cout<<sizeof(charptr)<<endl;
+
+    // long l=1040;
+    // long*longptr=&l;
+    // cout<<sizeof(longptr)<<endl;
+
+
+    // int a=5;
+    // int *ptr;
+    // cout<<*ptr<<endl;
+
+// declaration
+    // int *ptr=0;
+
+    // int a=10;
+    // int *p=&a;
+    // int *q=p;
+    // cout<<"a: "<<a<<endl;
+    //  cout<<"a: "<<&a<<endl;
+    //   cout<<"&a: "<<a<<endl;
+    // // cout<<"*a: "<<*a<<endl;
+    //     cout<<"p: "<<p<<endl;
+    //      cout<<"&p: "<<&p<<endl;
+    //       cout<<"*p: "<<*p<<endl;
+    //        cout<<"q: "<<q<<endl;
+    //         cout<<"&q: "<<&q<<endl;
+    //          cout<<"*q: "<<*q<<endl;
+
+
+    return 0; 
+}
