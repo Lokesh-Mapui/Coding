272c43337a9e0436653dd19389eefa18bcb34805
Lokesh Mapui <lokeshmapui88@gmail.com>
2024-08-24 17:15:59 +0530

    pointer class 2

---
+++ C++/supreme_3_O/pointer/code2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    // void solve(int* arr, int size){
+    //     arr[0]=1;
+    //     arr[1]=2;
+    //     arr[2]=20;
+    // }
+
+void solve(vector<int>&arr){
+    cout<<sizeof(arr)<<endl;
+}
+
+
+int main()
+{
+    vector<int>arr;
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    solve(arr);
+
+       
+    // int arr[]={10,20,30};
+    // int size=5;
+    // cout<<arr[0]<<arr[1]<<arr[2]<<endl;
+    // solve(arr,size);
+    // cout<<arr[0]<<arr[1]<<arr[2]<<endl;
+    return 0;
+}
