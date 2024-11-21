271cda507520132014f4e5929e316b677e1e7253
Lokesh Mapui <lokeshmapui88@gmail.com>
2024-11-21 21:09:06 +0530

    ll

---
+++ C/Dsa code/linkedlistTraversal.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node * next;
+};
+
+void linkedlisttraversal(struct node* ptr){
+    while(ptr!=NULL){
+        printf("Element: %d\n",ptr->data);
+        ptr=ptr->next;
+    }
+}
+
+int main()
+{
+    struct node * head;
+    struct node * second;
+    struct node * third;
+
+    // allocate memory in heap
+    head=(struct node *) malloc(sizeof(struct node));
+    second=(struct node *) malloc(sizeof(struct node));
+    third=(struct node *) malloc(sizeof(struct node));
+
+    // link first and second node
+    head->data =7;
+    head->next=second;
+
+    // link second and third node
+    second->data =11;
+    second ->next=third;
+
+    // terminate the list at third node
+    third -> data= 66;
+    third->next=NULL;
+
+    linkedlisttraversal(head);
+
+
+    return 0;
+}
