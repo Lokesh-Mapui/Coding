5c21d22fae5e5355d4821c8286424e1f23e923a1
Lokesh Mapui <lokeshmapui88@gmail.com>
2025-10-16 10:33:07 +0530

    assignments

---
+++ C++/Assignments/week2_assignments/numeric_hollow_inverted_half_pyramid.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+int n;
+cout<<"enter the number :";
+cin>>n;
+
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<=n;j++){
+        if(j==i+1 || j==n || i==0 ){
+            cout<<j;
+        }
+        else{
+            cout<<" ";
+        }
+    }
+    cout<<endl;
+}
+
+return 0;
+}
